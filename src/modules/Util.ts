f469623edcd2e356cdaff193f3b70cb1dc6cf0f4
bingoose0 <adamindrigo@gmail.com>
2023-05-24 23:31:58 -0400

    pricelist command

--- src/modules/Util.ts
+++ src/modules/Util.ts
@@ -1,19 +1,63 @@
-import { CacheType, Guild, ModalSubmitInteraction, SlashCommandSubcommandBuilder, TextBasedChannel, TextChannel, TextInputStyle } from "discord.js";
+import { APIEmbedField, AutoModerationActionExecution, EmbedBuilder, SlashCommandSubcommandBuilder } from "discord.js";
 import Module from "../Module";
-import { ActionRowBuilder, EmbedBuilder, ModalBuilder, TextInputBuilder } from "@discordjs/builders";
-import CurrentSale from "../util/CurrentSale";
-import { ItemMap } from "../util/Item";
-import Sale from "../models/Sale";
+import { ItemList } from "../util/Item";
+
 
 export default class Util extends Module {
     name = "Util"
-    channel: TextBasedChannel;
 
     createCommands(): void {
         const module = this;
 
         this.commands.push(
-            // Starts a sale
+            // Retreives the price list. VERY SHITTY CODE, must be optimized
+            {
+                name: "pricelist",
+                builder: new SlashCommandSubcommandBuilder()
+                    .setDescription("Shows the price list for Midnight Star"),
+                async executor(interaction) {
+                    const categories = {
+                        weapon: "Weaponry",
+                        ammo: "Ammunition",
+                        misc: "Miscellaneous"
+                        // attachment: "Attachments"
+                    }
+
+                    const fields = {};
+
+                    // Add items
+                    const embed = new EmbedBuilder();
+                    for(const key in ItemList) {
+                        const element = ItemList[key];
+                        const cat = categories[element.type];
+                        const field = fields[element.type];
+
+                        const itemText = element.name + " - " + element.price + "C\n"
+                        if(field) {
+                            field.value += itemText
+                        } else {
+                            const newField = {
+                                name: cat,
+                                value: itemText
+                            }
+
+                            fields[element.type] = newField
+                        }
+                    }
+
+                    const embedFields: Array<APIEmbedField> = []
+                    for(const key in fields) {
+                        const element = fields[key]
+                        embedFields.push(element)
+                    }
+    
+                    // Config embed
+                    embed.setTitle("Midnight Star - Prices")
+                    embed.addFields(...embedFields)
+    
+                    await interaction.reply({ embeds: [embed], ephemeral: true })
+                }
+            }
         )
     }
 }
