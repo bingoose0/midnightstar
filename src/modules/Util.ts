23b374030182693ae905f0fc5d5414cd853a20b7
bingoose0 <adamindrigo@gmail.com>
2023-05-25 20:16:31 -0400

    fix join message

--- src/modules/Util.ts
+++ src/modules/Util.ts
@@ -9,30 +9,8 @@ export default class Util extends Module {
     createCommands(): void {
         const module = this;
 
-        // God forgive me for this indentation
         const joinMessage = `
-***MIDNIGHT STAR - AN INTRODUCTION***
-
-Midnight Star is an arms dealing syndicate run by a mysterious woman known as The Sovereign, with the help of her board of Directors. Midnight Star is known as a community of professional arms dealers, ruthless terrorists, and iconic business owners. Lots of them are known by monikers they've earned with their experience. Here it is up to you to build a path in this group for your character, be what you want to be. We will give you the basics, and see what you do with them.
-        
-***LEGENDS OF THE REBELLION***
-        
-Midnight Star is home to a multitude of Legends. These people have pulled off insane feats, from storming nexus alone, to being rock stars, or helping lay entire cities to waste. They go by monikers, probably ones more synonymous than their real names. Stick with us, play your cards right, and carve your own spot as a legend.
-        
-***ARMS DEALERS FIRST***
-        
-Midnight Star primarily focuses on providing a consistent and reliable broker for weapons and other munitions to the rebellion. Whether that be other factions, or solo rebels and little friend groups. After you recieve your training you'll be placed as a Scav. What you do from there, whether it be an arms dealer yourself, or buy your way into Nightwatch with the blood on your hands, join the workforce, or simply gather materials. You keep what you earn, the group does not take cuts in anyway, so you make 100% of the profits from your sales. If you put in the work, you'll be swimming in money.
-        
-***TERRORISM ISN'T SOMETHING WE'RE SHY TO, THOUGH...***
-        
-If you decide to prove yourself bloodthirsty, where your primary desire is to wreak havoc, raid, and strike fear into the Combine, you may possibly qualify to join Nightwatch. Nightwatch is the team that specializes in coordinated acts of terrorism, as well as special and covert operations. It takes a lot to qualify, but once you're in, you're in. You need to be one nasty motherfucker to join the Demons in Blue. This isn't to say that your position as a Scavenger is free of causing havoc. You gotta prove your worthiness to the Nightwatch Directors somehow, and the means of doing so is up to you. Scavengers also like to seek action via hunting OTA, for those who seek a thrill, and high risk/high reward feats.
-        
-***THE BUSINESS FRONT***
-        
-Midnight Star is home to all kinds of iconic businesses that we put lots of love into. If you're interested, you can work at one and have some cool roleplay!
-        
-***That about sums up what our group is about...***
-If you're still interested, we'd love to have ya. We've got an alien empire to burn, and we'd like you to be there to help us.
+        <insert introduction message here>
         `
 
         this.commands.push(
@@ -182,6 +160,8 @@ If you're still interested, we'd love to have ya. We've got an alien empire to b
                 }
     
                 await member.roles.add(entryRoleID);
+
+                interaction.reply({ content: "Welcome!", ephemeral: true })
             }
         })
     }
